5e620c921988cf6f1d09d169f58cd4243909f1e8
Zatt010 <afuheho1@gmail.com>
2023-10-21 21:49:10 -0400

    Obtener detalles katas por  parametro de posicion

--- src/nombres.js
+++ src/nombres.js
@@ -1,5 +1,5 @@
 const nombres = ["KataBankOCR", "KataFizzBuzz", "FooBarQix", "KataPotter"];
-const detallesKatas = ["Detalles de la primera kata"];
+const detallesKatas = ["Detalles de la primera kata", "Detalles de la segunda kata", "Detalles de la tercera kata", "Detalles de la cuarta kata"];
 
 function obtenerNombres() {
   return nombres;
--- src/nombres.spec.js
+++ src/nombres.spec.js
@@ -11,4 +11,7 @@ describe("Obtener Detalles de Kata", () => {
   it("Obtener detalle de primera Kata", () => {
     expect(detallesKata(0)).toEqual("Detalles de la primera kata");
   });
+  it("Obtener detalle de la cuarta Kata", () => {
+    expect(detallesKata(3)).toEqual("Detalles de la cuarta kata");
+  });
 });
