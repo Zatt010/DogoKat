5aaa0ca4eb9b474c7dbfd642a0c6a4e29e7ce682
Zatt010 <afuheho1@gmail.com>
2023-10-21 22:11:09 -0400

    Manejo de clicks en la Kata y mostrar su detalle de la Kata presionada

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,22 @@
-import {obtenerNombres} from './nombres.js';
-
+import { obtenerNombres, detallesKata } from './nombres.js';
 document.addEventListener('DOMContentLoaded', () => {
   const nombresKatasDiv = document.querySelector('.nombres-katas');
-  //  nombres de las katas desde nombres.js
+  const detalleKataDiv = document.querySelector('.detalle-Kata');
   const nombres = obtenerNombres();
-  // mostrar los nombres en el HTML
-  nombresKatasDiv.innerHTML = `<ul>${nombres.map(nombre => `<li>${nombre}</li>`).join('')}</ul>`;
+
+  // Mostrar nombres en el HTML y manejo de los clics
+  nombresKatasDiv.innerHTML = `<ul>${nombres.map((nombre, index) => `<li><a href="#" data-kata="${index}">${nombre}</a></li>`).join('')}</ul>`;
+
+  nombresKatasDiv.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+      event.preventDefault();
+      const kataIndex = event.target.getAttribute('data-kata');
+      mostrarDetallesDeKata(kataIndex);
+    }
+  });
+
+  function mostrarDetallesDeKata(kataIndex) {
+    const detalles = detallesKata(kataIndex);
+    detalleKataDiv.innerHTML = detalles;
+  }
 });
