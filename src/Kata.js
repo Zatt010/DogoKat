2627982c7783a165d0602a78f0ea1e668ca61828
libia <libia.roman@ucb.edu.bo>
2023-10-24 17:59:19 +0200

    Agregar criterio de dificulta

--- src/Kata.js
+++ src/Kata.js
@@ -1,14 +1,16 @@
 class Kata{
-  constructor(nombre, detalle){
+  constructor(nombre, detalle, dificultad){
     this.nombre = nombre;
     this.detalle = detalle;
+    this.dificultad = dificultad;
   }
 }
+
 let Katas = [
-  new Kata("KataBankOCR", "Detalles de la primera kata"),
-  new Kata("KataFizzBuzz", "Detalles de la segunda kata"),
-  new Kata("FooBarQix", "Detalles de la tercera kata"),
-  new Kata("KataPotter", "Detalles de la cuarta kata")
+  new Kata("KataBankOCR", "Detalles de la primera kata","Principiante"),
+  new Kata("KataFizzBuzz", "Detalles de la segunda kata","Principiante"),
+  new Kata("FooBarQix", "Detalles de la tercera kata","Intermedio"),
+  new Kata("KataPotter", "Detalles de la cuarta kata","Avanzado")
 ]
 
 function obtenerNombres() {
@@ -19,9 +21,9 @@ function detallesKata(kataIndex) {
   return Katas[kataIndex].detalle;
 }
 
-function crearKata(nombreKata, detalleKata) {
+function crearKata(nombreKata, detalleKata, dificultadKata) {
   if (nombreKata.length <= 100) {
-    Katas.push(new Kata(nombreKata, detalleKata));
+    Katas.push(new Kata(nombreKata, detalleKata, dificultadKata));
     return true
   } else {
     return false;
