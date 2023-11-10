9bd5b3774128f16d9477eb3ca5dccbdc3a9e1769
Zatt010 <afuheho1@gmail.com>
2023-11-09 21:13:17 -0400

    Agregar pruebas en Cypress para verificar que la lista de nombres de katas se muestra correctamente

---
+++ cypress/e2e/Kata.cy.js
@@ -0,0 +1,8 @@
+it("Muestra la lista de nombres de katas", () => {
+    cy.visit("/");
+    const nombresEsperados = ["KataBankOCR", "KataFizzBuzz", "FooBarQix", "KataPotter"];
+    cy.get('.nombres-katas a').each(($enlace, index) => {
+        const nombreEsperado = nombresEsperados[index];
+        expect($enlace.text()).to.equal(nombreEsperado);
+    });
+});
