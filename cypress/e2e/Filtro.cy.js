b9411caf99c28f4b385d47de165ac5dfb88cc263
carQuis <carmen.quispe@ucb.edu.bo>
2023-11-11 21:11:59 -0400

    Categoria algoritmos

--- cypress/e2e/Filtro.cy.js
+++ cypress/e2e/Filtro.cy.js
@@ -8,6 +8,25 @@ describe("Mostrar katas por categoria", () => {
         cy.get(".nombres-katas").should("contain","KataBankOCR");
     
     });
+    it("Deberia mostrar todos los kata de la categoria 'Juegos'", () => {
+        cy.visit("/");
+            cy.get("#categoriaSelect").select("Juegos");
+        cy.get(".nombres-katas").should("contain","KataFizzBuzz");
+    
+    });
+    it("Deberia mostrar todos los kata de la categoria 'Tecnologia'", () => {
+        cy.visit("/");
+            cy.get("#categoriaSelect").select("Tecnologia");
+        cy.get(".nombres-katas").should("contain","FooBarQix");
+    
+    });
+    it("Deberia mostrar todos los kata de la categoria 'Algoritmos'", () => {
+        cy.visit("/");
+            cy.get("#categoriaSelect").select("Algoritmos");
+        cy.get(".nombres-katas").should("contain","KataPotter");
+    
+    });
+
 });
 
 describe("Mostrar katas por lenguaje", () => {
