ff0ba9fe98df612b7c74224f235e7bdb43af4bae
Zatt010 <afuheho1@gmail.com>
2023-11-20 19:47:52 -0400

    Implementacion del formulario para poder modificar una kata

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import { obtenerNombres, detallesKata, crearKata } from './Kata.js';
+import { obtenerNombres, detallesKata, crearKata, dificultadKata, categoriaKata,modificarKata,lenguajekata } from "./Kata.js";
 import { busquedaSimple } from './Busqueda.js';
 import { agruparKatasPorDificultad, agruparKatasPorCategoria, agruparKatasPorLenguaje } from './Filtros.js';
 
@@ -35,7 +35,11 @@ function inicializarApp() {
 
   function cargarNombres() {
     const nombres = obtenerNombres();
-    nombresKatasDiv.innerHTML = `<ul>${nombres.map((nombre, index) => `<li><a href="#" data-kata="${index}">${nombre}</a></li>`).join('')}</ul>`;
+    nombresKatasDiv.innerHTML = `<ul>${nombres.map((nombre, index) => `
+      <li>
+        <a href="#" data-kata="${index}">${nombre}</a>
+        <button class="modificar-button" data-kata="${index}">Modificar</button>
+      </li>`).join('')}</ul>`;
     nombresKatasDiv.addEventListener('click', manejarClickNombre);
   }
 
@@ -55,6 +59,15 @@ function inicializarApp() {
     } else {
       nombresKatasDiv.innerHTML = 'Kata no encontrada';
     }
+  }
+
+  function mostrarDetallesDeKata(kataIndex) {
+    const detalles = detallesKata(kataIndex);
+    detalleKataDiv.innerHTML = detalles;
+  }
+
+  function capitalize(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
   }
 
   function cargarFormularioKata() {
@@ -94,13 +107,53 @@ function inicializarApp() {
       alert('Error al crear la Kata');
     }
   }
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('modificar-button')) {
+      event.preventDefault();
+      const kataIndex = event.target.getAttribute('data-kata');
+      cargarFormularioModificacion(kataIndex);
+    }
+  });
 
-  function mostrarDetallesDeKata(kataIndex) {
-    const detalles = detallesKata(kataIndex);
-    detalleKataDiv.innerHTML = detalles;
+  function cargarFormularioModificacion(kataIndex) {
+    fetch('./modificarKata.html')
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById('formulario-kata').innerHTML = html;
+        document.getElementById('index').value = kataIndex;
+        document.getElementById('nuevoNombre').value = obtenerNombres()[kataIndex];
+        document.getElementById('nuevoDetalle').value = detallesKata(kataIndex);
+        document.getElementById('nuevaDificultad').value = dificultadKata(kataIndex);
+        document.getElementById('nuevaCategoria').value = categoriaKata(kataIndex);
+        document.getElementById('nuevoLenguaje').value = lenguajekata(kataIndex);
+
+        const modificarKataForm = document.getElementById('modificar-kata-form');
+        modificarKataForm.addEventListener('submit', manejarSubmitModificacion);
+      })
+      .catch(error => console.error('Error al cargar el formulario:', error));
   }
 
-  function capitalize(text) {
-    return text.charAt(0).toUpperCase() + text.slice(1);
+  function manejarSubmitModificacion(event) {
+    event.preventDefault();
+
+    const index = document.getElementById('index').value;
+    const nuevoNombre = document.getElementById('nuevoNombre').value;
+    const nuevoDetalle = document.getElementById('nuevoDetalle').value;
+    const nuevaDificultad = document.getElementById('nuevaDificultad').value;
+    const nuevaCategoria = document.getElementById('nuevaCategoria').value;
+    const nuevoLenguaje = document.getElementById('nuevoLenguaje').value;
+
+    const exitoModificacion = modificarKata(index, nuevoNombre, nuevoDetalle, nuevaDificultad, nuevaCategoria, nuevoLenguaje);
+    console.log('index');
+
+    if (exitoModificacion) {
+      alert('Kata modificada exitosamente.');
+      const formularioKata = document.getElementById('formulario-kata');
+      formularioKata.style.display = 'none';
+      cargarNombres();
+    } else {
+      alert('No se pudo modificar la Kata. Verifica el índice proporcionado.');
+    }
   }
+
 }
