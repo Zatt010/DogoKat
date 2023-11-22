718e33e09a5b567677cc02b97e926eea232934ad
carQuis <carmen.quispe@ucb.edu.bo>
2023-11-22 10:07:43 -0400

    CursoCreado

---
+++ src/Curso.spec.js
@@ -0,0 +1,12 @@
+import { obtenerNombres, detallesCurso, crearCurso } from "./Curso.js";
+
+describe("Creacion de Curso", () => {
+    it("Obtener Posicion de la nueva Curso", () => {
+      const nombreCurso = "IngeSofw";
+      const detalleCurso = "Nuevo curso del semestre";
+      const resultado = crearCurso(nombreCurso, detalleCurso);
+      expect(resultado).toBe(true);
+    });
+  
+});
+  
