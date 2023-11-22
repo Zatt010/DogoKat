185d957ab2fff7cff9fc0379107dd9284b9a6fde
Zatt010 <afuheho1@gmail.com>
2023-11-22 08:42:03 -0400

    Refactorizacion, separacion de las funciones de formulario de kata a un js formulario.js para liberar el presenter.js

--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,7 @@
 import { obtenerNombres, detallesKata, crearKata, dificultadKata, categoriaKata,modificarKata,lenguajekata } from "./Kata.js";
 import { busquedaSimple } from './Busqueda.js';
 import { agruparKatasPorDificultad, agruparKatasPorCategoria, agruparKatasPorLenguaje } from './Filtros.js';
-
+import * as formularios from './formulariosKata.js';
 document.addEventListener('DOMContentLoaded', inicializarApp);
 
 function inicializarApp() {
@@ -16,7 +16,6 @@ function inicializarApp() {
 
   cargarNombres();
 
-  crearKataButton.addEventListener('click', cargarFormularioKata);
   busquedaButton.addEventListener('click', realizarBusqueda);
 
   dificultadSelect.addEventListener('change', () => mostrarKatasPorCriterio(dificultadSelect.value, agruparKatasPorDificultad()));
@@ -34,7 +33,7 @@ function inicializarApp() {
   }
 
   function cargarNombres() {
-    const nombres = obtenerNombres();
+    let nombres = obtenerNombres();
     nombresKatasDiv.innerHTML = `<ul>${nombres.map((nombre, index) => `
       <li>
         <a href="#" data-kata="${index}">${nombre}</a>
@@ -70,90 +69,31 @@ function inicializarApp() {
     return text.charAt(0).toUpperCase() + text.slice(1);
   }
 
-  function cargarFormularioKata() {
-    fetch('./creacionKata.html')
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById('formulario-kata').innerHTML = html;
-        const crearKataForm = document.getElementById('kata-form');
-        crearKataForm.addEventListener('submit', manejarSubmitFormulario);
-      })
-      .catch(error => console.error('Error al cargar el formulario:', error));
+// Función común para manejar la lógica de carga de formularios
+function manejarCargaFormulario(promesa) {
+  promesa
+    .then(resultado => {
+      if (resultado) {
+        console.log('recibido', resultado);
+        cargarNombres();
+      } else {
+        console.log('no recibido', resultado);
+      }
+    })
+    .catch(error => {
+      console.error('Error al cargar el formulario:', error);
+    });
+}
+
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('modificar-button')) {
+    console.log('presionando modificar');
+    const kataIndex = event.target.getAttribute('data-kata');
+    manejarCargaFormulario(formularios.cargarFormularioModificacion(kataIndex), 'Formulario de modificación cargado con éxito');
   }
+});
 
-  function manejarSubmitFormulario(event) {
-    event.preventDefault();
-
-    const nombreKataInput = document.getElementById('nombre');
-    const detalleKataInput = document.getElementById('detalle');
-    const dificultadKataSelect = document.getElementById('dificultad');
-    const categoriaKataSelect = document.getElementById('categoria');
-    const lenguajeKataSelect = document.getElementById('lenguaje');
-
-    const nombreKata = nombreKataInput.value;
-    const detalleKata = detalleKataInput.value;
-    const dificultadKata = dificultadKataSelect.value;
-    const categoriaKata = categoriaKataSelect.value;
-    const lenguajeKata = lenguajeKataSelect.value;
-
-    const kataCreada = crearKata(nombreKata, detalleKata, dificultadKata, categoriaKata, lenguajeKata);
-
-    if (kataCreada) {
-      alert('Kata creada con éxito');
-      const formularioKata = document.getElementById('formulario-kata');
-      formularioKata.style.display = 'none';
-      cargarNombres();
-    } else {
-      alert('Error al crear la Kata');
-    }
-  }
-  document.addEventListener('click', function (event) {
-    if (event.target.classList.contains('modificar-button')) {
-      event.preventDefault();
-      const kataIndex = event.target.getAttribute('data-kata');
-      cargarFormularioModificacion(kataIndex);
-    }
-  });
-
-  function cargarFormularioModificacion(kataIndex) {
-    fetch('./modificarKata.html')
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById('formulario-kata').innerHTML = html;
-        document.getElementById('index').value = kataIndex;
-        document.getElementById('nuevoNombre').value = obtenerNombres()[kataIndex];
-        document.getElementById('nuevoDetalle').value = detallesKata(kataIndex);
-        document.getElementById('nuevaDificultad').value = dificultadKata(kataIndex);
-        document.getElementById('nuevaCategoria').value = categoriaKata(kataIndex);
-        document.getElementById('nuevoLenguaje').value = lenguajekata(kataIndex);
-
-        const modificarKataForm = document.getElementById('modificar-kata-form');
-        modificarKataForm.addEventListener('submit', manejarSubmitModificacion);
-      })
-      .catch(error => console.error('Error al cargar el formulario:', error));
-  }
-
-  function manejarSubmitModificacion(event) {
-    event.preventDefault();
-
-    const index = document.getElementById('index').value;
-    const nuevoNombre = document.getElementById('nuevoNombre').value;
-    const nuevoDetalle = document.getElementById('nuevoDetalle').value;
-    const nuevaDificultad = document.getElementById('nuevaDificultad').value;
-    const nuevaCategoria = document.getElementById('nuevaCategoria').value;
-    const nuevoLenguaje = document.getElementById('nuevoLenguaje').value;
-
-    const exitoModificacion = modificarKata(index, nuevoNombre, nuevoDetalle, nuevaDificultad, nuevaCategoria, nuevoLenguaje);
-    console.log('index');
-
-    if (exitoModificacion) {
-      alert('Kata modificada exitosamente.');
-      const formularioKata = document.getElementById('formulario-kata');
-      formularioKata.style.display = 'none';
-      cargarNombres();
-    } else {
-      alert('No se pudo modificar la Kata. Verifica el índice proporcionado.');
-    }
-  }
-
+crearKataButton.addEventListener('click', function() {
+  manejarCargaFormulario(formularios.cargarFormularioCreacionKata(), 'Formulario de creación cargado con éxito');
+});
 }
