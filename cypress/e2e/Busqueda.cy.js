f0b64bee894494af3df1f6085c6f6bcfcf848922
Adriel Fernandez <adriel.fernandez@ucb.edu.bo>
2023-11-23 02:07:29 -0400

    Mostrar resultados de busqueda

--- cypress/e2e/Busqueda.cy.js
+++ cypress/e2e/Busqueda.cy.js
@@ -8,13 +8,21 @@ describe("Criterios de busqueda", () => {
         .should("contain", "KataFizzBuzz")
         .and("contain", "FooBarQix");
     });
+});
+describe("Criterios de busqueda", () => {
+    it("Mostrar resultados de busqueda", () => {
+        cy.visit("/");
+        cy.get('a[href="/maestro"]').click();
+
+        cy.get("#busquedaInput").type("f");
+        cy.get("#busquedaButton").click();
+        cy.get(".nombres-katas")
+        .should("contain", "KataFizzBuzz")
+        .and("contain", "FooBarQix");
+    });
     it("Mostrar busqueda no encontrada", () => {
         cy.visit("/");
-        cy.get("#username").type("maestro");
-        cy.get("#password").type("123");
-        cy.get("#role").select("maestro");
-        cy.get("#login-button").click();
-        cy.url().should("include", "/maestro.html");
+        cy.get('a[href="/maestro"]').click();
         cy.get("#busquedaInput").type("m");
         cy.get("#busquedaButton").click();
         cy.get(".nombres-katas").should("contain", "Kata no encontrada");
